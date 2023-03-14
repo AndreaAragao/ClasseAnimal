b1d9c48ee24c3ac3abdd8ac85c3386e0b4efff98
Andréa <andreamaragao@gmail.com>
2023-03-13 22:31:37 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto01.Entidade
+{
+    public class Animal
+    {
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Projeto01.Entidade;
+using System;
+
+namespace Projeto01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal meuAnimal;
+            meuAnimal = new Animal();
+
+            meuAnimal.Nome = "bob";
+            meuAnimal.Raca = "cachorro";
+
+            Animal seuAnimal;
+            seuAnimal = new Animal();
+            seuAnimal.Nome = "nenem";
+            seuAnimal.Raca = "papagaio";
+
+            Animal nossoAnimal;
+            nossoAnimal = new Animal();
+            nossoAnimal.Nome = "ping";
+            nossoAnimal.Raca = "tucano";
+
+        }
+    }
+}
